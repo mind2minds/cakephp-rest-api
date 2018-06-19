66d3e81007b3b7cf9a9e2fccb6df443995cc8d0e
DilshadKhan <khan.dil@gmail.com>
2018-06-19 17:05:32 +0530

    fined tune api keys

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 //DEMO API KEYS
 $demoApiKeys = [
-    's91Q-7Nq20c-68J4OO-0Jj39T-72WY',
-    '1111-222222-333333-444444-5555',
-    'Y48c-wBXwtF-o9HeSq-Sznbmw-Y48c'
+    '741f3a97-e0c4-491c-b702-cbea0975f88e',
+    'ad5b2ab4-3688-4ba5-9a02-56120ea4b7d3',
+    '1de88309-27ba-4416-ac15-c0d51c53ba6e'
 ];
 
 \Cake\Core\Configure::write('RESTAPI.APIKEYS', $demoApiKeys);
